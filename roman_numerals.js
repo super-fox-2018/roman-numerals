ee97ea566012cf7e591d297ceea053ffd90b97f1
andrewsasmita <its.me.andrew.sasmita@gmail.com>
2018-04-30 16:57:36 +0700

    finished, but too long

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,61 @@
 function to_roman (num) {
-  // your implementation code here
+  
+    var numerals = ['I','V','X','L','C','D','M']
+    var numeralValue = [1,5,10,50,100,500,1000]
+    var number = num 
+    var result =''
+
+    for(x=0; x<100; x++) {
+
+        if(number>=1000) {
+            result += numerals[6]
+            number -= 1000
+        }   else if(number>=900) {
+            result += numerals[4]
+            result += numerals[6]
+            number -= 900
+        }   else if(number>=500) {
+            result += numerals[5]
+            number -= 500
+        }   else if(number>=400) {
+            result += numerals[4]
+            result += numerals[5]
+            number -= 400
+        }   else if(number>=100) {
+            result += numerals[4]
+            number -= 100
+        }   else if(number>=90) {
+            result += numerals[2]
+            result += numerals[4]
+            number -= 90
+        }   else if(number>=50) {
+            result += numerals[3]
+            number -= 50
+        }   else if(number>=40) {
+            result += numerals[2]
+            result += numerals[3]
+            number -= 40
+        }   else if(number>=10) {
+            result += numerals[2]
+            number -= 10
+        }   else if(number>=9) {
+            result += numerals[0]
+            result += numerals[2]
+            number -= 9
+        }   else if(number>=5) {
+            result += numerals[1]
+            number -= 5
+        }   else if(number>=4) {
+            result += numerals[0]
+            result += numerals[1]
+            number -= 4
+        }   else if(number>=1) {
+            result += numerals[0]
+            number -= 1
+        }     
+    } 
+    
+    return result
 }
 
 // Drive code
