4cf408eea958b3a7529ef643d9fa359fe0b4abf6
Dwiky Erlangga <dwikyerl@gmail.com>
2018-04-30 16:12:32 +0700

    Finish Roman Numerals Challenge

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,16 @@
 function to_roman (num) {
-  // your implementation code here
+  const romans = { 1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'};
+  let roman = '';
+  const numbers = Object.keys(romans);
+  let i = numbers.length - 1;
+  while (num !== 0) {
+    if (num - numbers[i] >= 0) {
+      num -= numbers[i];
+      roman += romans[numbers[i]];
+    } else i -= 1;
+  }
+
+  return roman;
 }
 
 // Drive code
