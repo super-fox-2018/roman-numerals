7d7a6872f3293a71f7f73620c6c14506c15d7abc
Rhesa Utomo <venderblitz@gmail.com>
2018-04-30 16:37:33 +0700

    first commit, more effective way on progress

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,70 @@
 function to_roman (num) {
   // your implementation code here
+
+  // var arr = String(num);
+  var roman = [];
+
+  var numerals = { 1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L',
+                  90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M' }
+
+  while (num > 0) {
+    if (num >= 1000) {
+      roman.push(numerals[1000]);
+      num -= 1000;
+    }
+    else if (num >= 900 && num < 1000) {
+      roman.push(numerals[900]);
+      num -= 900;
+    }
+    else if (num >= 500 && num < 900) {
+      roman.push(numerals[500]);
+      num -= 500;
+    }
+    else if (num >= 400 && num < 500) {
+      roman.push(numerals[400]);
+      num -= 400;
+    }
+    else if (num >= 100 && num < 400) {
+      roman.push(numerals[100]);
+      num -= 100;
+    }
+    else if (num >= 90 && num < 100) {
+      roman.push(numerals[90]);
+      num -= 90;
+    }
+    else if (num >= 50 && num < 90) {
+      roman.push(numerals[50]);
+      num -= 50;
+    }
+    else if (num >= 40 && num < 50) {
+      roman.push(numerals[40]);
+      num -= 40;
+    }
+    else if (num >= 10 && num < 40) {
+      roman.push(numerals[10]);
+      num -= 10;
+    }
+    else if (num === 9) {
+      roman.push(numerals[9]);
+      num -= 9;
+    }
+    else if (num >= 5 && num < 9) {
+      roman.push(numerals[5]);
+      num -= 5
+    }
+    else if (num === 4) {
+      roman.push(numerals[4]);
+      num -= 4
+    }
+    else if (num >= 1 && num < 4) {
+      roman.push(numerals[1]);
+      num -= 1
+    }
+  }
+
+
+  var display = roman.join('');
+  return display;
 }
 
 // Drive code
